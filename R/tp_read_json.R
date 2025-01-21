e1488d67b54473b928b7068578489a5e933bc68f
Giorgio Comai <g@giorgiocomai.eu>
2025-01-21 23:45:14 +0100

    introduce selector to facilitate retrieving channels

--- R/tp_read_json.R
+++ R/tp_read_json.R
@@ -7,19 +7,35 @@
 #'
 #' @examples
 tp_read_json <- function(path) {
-  channel_l <- yyjsonr::read_json_file(filename = path)
 
-  channel_full_df <- channel_l[["messages"]] |>
-    tibble::as_tibble()
+  if (fs::is_dir(path)) {
+    path <- fs::dir_ls(
+      path = path,
+      recurse = FALSE,
+      type = "file",
+      glob = "*.json"
+    )
+  }
 
-  channel_info_df <- tibble::tibble(
-    channel_name = channel_l[["name"]],
-    channel_type = channel_l[["type"]],
-    channel_id = channel_l[["id"]]
-  )
+  purrr::map(
+    .progress = "Reading archives",
+    .x = path, .f = \(current_path) {
+      channel_l <- yyjsonr::read_json_file(filename = current_path)
 
-  dplyr::bind_cols(
-    channel_info_df,
-    channel_full_df
-  )
+      channel_full_df <- channel_l[["messages"]] |>
+        tibble::as_tibble()
+
+      channel_info_df <- tibble::tibble(
+        channel_name = channel_l[["name"]],
+        channel_type = channel_l[["type"]],
+        channel_id = channel_l[["id"]]
+      )
+
+      dplyr::bind_cols(
+        channel_info_df,
+        channel_full_df
+      )
+    }) |>
+    purrr::list_rbind() |>
+    dplyr::distinct(channel_id, id, .keep_all = TRUE)
 }
---
+++ R/tp_select.R
@@ -0,0 +1,43 @@
+#' Select a Telegram channel from a folder where multiple archives are stored
+#'
+#' @param path Path to a folder where Telegram archives in json format have
+#'   previously been stored.
+#' @param channel_name Defaults to NULL. Name of the channel, must match
+#'   exactly. Be aware that, in principle, different channels can have exactly
+#'   the same name or can change name, `channel_id` is generally safer.
+#' @param channel_id Defaults to NULL. Numeric identifier of the channel, must
+#'   match exactly.
+#'
+#' @returns A vector of paths to json files corresponding to the relevant
+#'   Telegram channel.
+#' @export
+#'
+#' @examples
+tp_select <- function(path,
+                      channel_name = NULL,
+                      channel_id = NULL) {
+  if (fs::is_dir(path) == FALSE) {
+    cli::cli_abort("{.var path} must be a folder, and {.path {path}} isn't.")
+  }
+
+  metadata_df <- tp_get_metadata(path = path)
+
+  if (is.null(channel_name) & is.null(channel_id)) {
+    selected_df <- metadata_df
+  } else if (is.null(channel_name) == FALSE) {
+    selected_df <- metadata_df |>
+      dplyr::filter(!!channel_name == channel_name)
+  } else if (is.null(channel_id) == FALSE) {
+    selected_df <- metadata_df |>
+      dplyr::filter(!!channel_id == channel_id)
+  } else {
+    cli::cli_abort("Either {.var channel_name} or {.var channel_id} must be provided.")
+  }
+
+  selected_df |>
+    dplyr::left_join(
+      y = tibble::tibble(full_path = fs::dir_ls(path = path)) |>
+        dplyr::mutate(filename = fs::path_file(full_path)),
+      by = "filename") |>
+    dplyr::pull(full_path)
+}
